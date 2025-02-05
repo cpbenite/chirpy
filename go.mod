2944befe3308497ea510ca7083b8a4e200559ef3
Christian Benitez <christian.p.benitez@gmail.com>
2025-02-04 19:32:42 -0800

    Initial server architecture.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cpbenite/chirpy
+
+go 1.23.0
---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+	cfg := apiConfig{}
+
+	mux := http.NewServeMux()
+	mux.Handle("/app/", cfg.middlewareMetricsInc(fileServerHandler()))
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /admin/metrics", cfg.requestHitsHandlerFunc)
+	mux.HandleFunc("POST /admin/reset", cfg.resetHitsHandlerFunc)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
+}
+
+func fileServerHandler() http.Handler {
+	return http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) requestHitsHandlerFunc(w http.ResponseWriter, r *http.Request) {
+	htmlTemplate := `<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`
+
+	res := fmt.Sprintf(htmlTemplate, cfg.fileserverHits.Load())
+
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(res))
+}
+
+func (cfg *apiConfig) resetHitsHandlerFunc(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
